585a1389bc9a9c674f1f1a52ce7bafa7732160b8
Sangmin <tkdals730@naver.com>
2026-05-13 17:44:37 +0900

    fix : 투표결과를 뷰플리퍼를 이용해 일정 시간마다 보이게 변경

--- 402p6/app/src/main/java/com/example/a402p6/ResultActivity.java
+++ 402p6/app/src/main/java/com/example/a402p6/ResultActivity.java
@@ -1,33 +1,31 @@
 package com.example.a402p6;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RatingBar;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import androidx.annotation.Nullable;
 
 public class ResultActivity extends Activity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
+
         setTitle("투표 결과");
 
         Intent intent = getIntent();
+
         int[] voteResult = intent.getIntArrayExtra("VoteCount");
         String[] imageName = intent.getStringArrayExtra("ImageName");
-
-        TextView tv[] = new  TextView[imageName.length];
-        RatingBar rbar[] = new RatingBar[imageName.length];
-
-        Integer tvID[] = {R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5, R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9};
-        Integer rbarID[] = {R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4, R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9};
 
         Integer imageFileId[] = {
                 R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
@@ -35,28 +33,90 @@ public class ResultActivity extends Activity {
                 R.drawable.pic7, R.drawable.pic8, R.drawable.pic9
         };
 
-        int maxIndex = 0;
+        ViewFlipper viewFlipper =
+                (ViewFlipper) findViewById(R.id.viewFlipper);
 
-        for (int i = 0 ; i < voteResult.length ; i++){
-            tv[i] = (TextView) findViewById(tvID[i]);
-            rbar[i] = (RatingBar) findViewById(rbarID[i]);
+        // 순위 저장용 배열
+        Integer rank[] = {0,1,2,3,4,5,6,7,8};
 
-            tv[i].setText(imageName[i]);
-            rbar[i].setRating((float) voteResult[i]);
+        // 득표수 기준 정렬
+        for (int i = 0; i < rank.length - 1; i++) {
+            for (int j = i + 1; j < rank.length; j++) {
 
-            if (voteResult[i] > voteResult[maxIndex]) {
-                maxIndex = i;
+                if (voteResult[rank[i]] < voteResult[rank[j]]) {
+
+                    int temp = rank[i];
+                    rank[i] = rank[j];
+                    rank[j] = temp;
+                }
             }
         }
-        TextView tvTop = (TextView) findViewById(R.id.tvTop);
-        ImageView ivTop = (ImageView) findViewById(R.id.ivTop);
 
-        tvTop.setText(imageName[maxIndex]);
-        ivTop.setImageResource(imageFileId[maxIndex]);
-        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        // ViewFlipper에 추가
+        for (int i = 0; i < rank.length; i++) {
+
+            int index = rank[i];
+
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+            layout.setGravity(Gravity.CENTER);
+
+            TextView tvInfo = new TextView(this);
+
+            tvInfo.setText(
+                    (i + 1) + "등 : "
+                            + imageName[index]
+                            + "\n득표수 : "
+                            + voteResult[index]
+                            + "표"
+            );
+
+            tvInfo.setTextSize(24);
+            tvInfo.setGravity(Gravity.CENTER);
+
+            ImageView imageView = new ImageView(this);
+
+            imageView.setImageResource(imageFileId[index]);
+
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+            imageView.setAdjustViewBounds(true);
+
+            layout.addView(tvInfo);
+            layout.addView(imageView);
+
+            viewFlipper.addView(layout);
+        }
+
+        Button btnStart =
+                (Button) findViewById(R.id.btnStart);
+
+        Button btnStop =
+                (Button) findViewById(R.id.btnStop);
+
+        Button btnReturn =
+                (Button) findViewById(R.id.btnReturn);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                viewFlipper.startFlipping();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                viewFlipper.stopFlipping();
+            }
+        });
+
         btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 finish();
             }
         });
